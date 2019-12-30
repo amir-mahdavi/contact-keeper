c79c3e70a0b94e83c6f8340e231a80a9586cf5f3
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-12-30 16:24:57 +1100

    user saved

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ connectDB();
 
 const app = express();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ message: 'hi there' }));
 
 app.use('/api/auth', require('./routes/auth'));

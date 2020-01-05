c90b937acc0e4e7922151100ededed5e77cb6300
Amir Mahdavi <amirmahdavi65@gmail.com>
2020-01-05 13:45:13 +1100

    Delete contact done!

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -39,11 +39,16 @@ const ContactState = props => {
     dispatch({ type: ADD_CONTACT, payload: contact });
   };
 
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
